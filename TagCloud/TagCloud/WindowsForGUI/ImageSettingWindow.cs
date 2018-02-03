085c0b0cf0b69f0f89609af47ca423e1d0294ff1
Arthur Karamow <ybiz177@gmail.com>
2018-02-03 17:47:02 +0600

    Added Image setting

--- TagCloud/TagCloud/WindowsForGUI/ImageSettingWindow.cs
+++ TagCloud/TagCloud/WindowsForGUI/ImageSettingWindow.cs
@@ -1,12 +1,16 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace TagCloud.WindowsForGUI
 {
     internal class ImageSettingWindow : Form
     {
+        private ProgrammArgumentsValue values;
+
         public ImageSettingWindow(ProgrammArgumentsValue values)
         {
+            this.values = values;
             var padx = 25;
             var pady = 75;
             var dx = 100;
@@ -28,7 +32,7 @@ namespace TagCloud.WindowsForGUI
                 Text = "Width:"
             };
 
-            var textWidth =new TextBox();
+            var textWidth =new TextBox(){Text = "800"};
             labelWidth.Bounds = new Rectangle(padx, pady, size.Width, size.Height);
             textWidth.Bounds = new Rectangle(padx + dx, pady, size.Width, size.Height);
             Controls.Add(textWidth);
@@ -43,7 +47,7 @@ namespace TagCloud.WindowsForGUI
                 Font =  font
             };
 
-            var textHeight = new TextBox();
+            var textHeight = new TextBox(){Text = "600"};
             labelHeight.Bounds = new Rectangle(padx, pady+=dy, size.Width, size.Height);
             textHeight.Bounds = new Rectangle(padx + dx, pady, size.Width, size.Height);
             Controls.Add(labelHeight);
@@ -109,17 +113,36 @@ namespace TagCloud.WindowsForGUI
 
         private void Accept(string widthWindow, string heightWindow, Color backgroundColor, Color textColor)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                values.Width = int.Parse(widthWindow);
+                values.Heigth = int.Parse(heightWindow);
+                values.Color = textColor;
+                this.Hide();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(
+                    "One or more parameters were not specified correctly, please check the correctness of the data",
+                    "Invalid parameters");
+            };
         }
 
         private void CheangeTextColor(object sender)
         {
-            throw new System.NotImplementedException();
+            var colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            (sender as Button).ForeColor = colorDialog.Color;
         }
 
         private void CheangeColor(object sender)
         {
-            throw new System.NotImplementedException();
+            var colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            (sender as Button).BackColor = colorDialog.Color;
         }
     }
 }
+    
